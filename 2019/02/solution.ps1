c0454877c329e4c0d52132f1c9c4d59dc7c50358
Jonathan Setzer <jonathan.setzer@gmail.com>
2019-12-11 16:59:42 -0600

    solved day 2 part 2

--- 2019/02/solution.ps1
+++ 2019/02/solution.ps1
@@ -1,7 +1,8 @@
-[int[]]$InputArray = (Get-Content "$PSScriptRoot\input.txt") -split ','
+function Get-PuzzleInput {
+    [int[]]$InputArray = (Get-Content "$PSScriptRoot\input.txt") -split ','
+    $InputArray
+}
 
-$InputArray[1] = 12
-$InputArray[2] = 2
 
 function Invoke-Intcode {
     [CmdletBinding()]
@@ -12,7 +13,7 @@ function Invoke-Intcode {
     )
     
     begin {
-        $Memory = $InputArray
+        $Memory = $Opcodes
     }
     
     process {
@@ -35,42 +36,72 @@ function Invoke-Intcode {
                     Write-Output "99, program complete."
                 }
                 Default {
-                    Write-Error "Invalid op code $_ at $Position."
+                    # Write-Error "Invalid op code $_ at $Position."
                     return
                 }
             } 
         } until ($Memory[$Position] -eq 99)
-        return [PSCustomObject]@{
-                Memory = $Memory[0]
-                FinalPosition = $Position
-                 }
+        return [PSCustomObject]@{ 
+                                Memory = $Memory; 
+                                FinalPosition = $Position 
+                            }
     }
 }
 
-Invoke-Intcode -OpCodes $InputArray
+# $test = Get-PuzzleInput
+# Invoke-Intcode -OpCodes $test
+# $run.Memory[0]
+
+function Invoke-IntcodeSolver ($Noun, $Verb){
+    $PuzzleInput = Get-PuzzleInput
+    $PuzzleInput[1] = $Noun
+    $PuzzleInput[2] = $Verb
+    $Output = Invoke-Intcode -OpCodes $PuzzleInput
+    Return $Output.Memory[0]
+}
+
+
+
+# Invoke-IntcodeSolver -Noun 12 -Verb 2
+# Invoke-IntcodeSolver -Noun 0 -Verb 0
+# Invoke-IntcodeSolver
+    
+    for ($i = 0; $i -le 99; $i++) {
+        for ($j = 0; $j -le 99; $j++){
+            # Write-Output "Noun of $i and Verb of $j being used"
+            $BruteForce = Invoke-IntcodeSolver -Noun $i -Verb $j
+            if ($BruteForce -eq 19690720){
+                return Write-Output (100 * $i + $j)
+            }
+            
+        }
+    }
+    
+
+
 
 # Keyterms
 # Address
 # Memory
 # Parameters
 # Instruction
-$Memory = $InputArray
+# $Memory = $InputArray
 
-$Address = 0
+# $Address = 0
 
-$Memory[$Address]
+# $Memory[$Address]
 
-$Opcodes = @(1,2,99)
+# $Opcodes = @(1,2,99)
 
 
-$Opcode = 0
-$Parameter1 = $Opcode+1
-$Parameter2 = $Opcode+2
-$Parameter3 = $Opcode+3
+# $Opcode = 0
+# $Parameter1 = $Opcode+1
+# $Parameter2 = $Opcode+2
+# $Parameter3 = $Opcode+3
 
-$Instruction = @($Opcode, $Parameter1, $Parameter2, $Parameter3)
-$InstructionIncrement = $Instruction.Count
+# $Instruction = @($Opcode, $Parameter1, $Parameter2, $Parameter3)
+# $InstructionIncrement = $Instruction.Count
 
-$InstructionPointer = 0 
-$InstructionPointer = $InstructionPointer + $InstructionIncrement
+# $InstructionPointer = 0 
+# $InstructionPointer = $InstructionPointer + $InstructionIncrement
 
