29bd8ee0bd35e10ffa5d872a1ce9981368ebb9a5
Jonathan Setzer <jonathan.setzer@gmail.com>
2020-09-04 19:17:18 -0500

    Modify solution output

--- 2015/02/solution.py
+++ 2015/02/solution.py
@@ -32,7 +32,7 @@ with open('input.txt', 'r') as f:
         area = (2*l*w) + (2*w*h) + (2*h*l)
         slack = min(l*w, w*h, h*l)
         total += area + slack
-        print(total)
+    print(total)
 
 """
 --- Part Two ---
